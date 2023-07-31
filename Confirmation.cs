fc9a9c442c6af3ecde14d9b1cbba81c83b39c1cf
Alan Rai <33865710+ALANRAI@users.noreply.github.com>
2023-07-31 06:19:27 +0000

    API endpoints with data models

---
+++ Confirmation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MFA_assignment.Models
+{
+    public class Confirmation
+    {
+        // Request model for sending confirmation code
+        public class SendConfirmationCodeRequest
+        {
+            public string Phone { get; set; }
+        }
+
+        // Request model for checking confirmation code
+        public class CheckConfirmationCodeRequest
+        {
+            public string Phone { get; set; }
+            public string Code { get; set; }
+        }
+
+        // Response model for both endpoints
+        public class ApiResponse
+        {
+            public bool Sent { get; set; }
+            public bool Valid { get; set; }
+            public string ErrorMessage { get; set; }
+            public string GetThisCodeonPhone { get; set; }
+        }
+
+    }
+}
